66ab6398b27e429d081a4b0f7c8b7d6216bfa168
Bharti Basumatary <bhartibasumatary61040@gmail.com>
2026-02-27 02:15:50 +0530

    Initial commit

---
+++ src/coreJava/PS.java
@@ -0,0 +1,25 @@
+package coreJava;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class PS {
+
+	public void doThis() {
+		
+		System.out.println("I am here in PS class");
+	}
+	
+	 @BeforeMethod
+	    
+	    public void beforeRun() {
+	    	System.out.println("Run me first");
+	    }
+	 
+	 @AfterMethod
+	    
+	    public void afterRun() {
+	    	System.out.println("Run me last");
+	    }
+
+}
---
+++ src/coreJava/PS2.java
@@ -0,0 +1,24 @@
+package coreJava;
+
+public class PS2 extends PS3 {
+	
+	int a ;
+	
+	//default constructor
+	public PS2(int a) {
+		super(a); // parent class constructor is invoked
+		this.a= a;
+	}
+
+
+	public int increment() {
+		a= a+1;
+		return a ;
+		
+	}
+	
+	public int decrement() {
+		a= a-1;
+		return a ;
+}
+}
